b498cefde29274fdcb7ae5a1e7a81b8f4255f619
Aris221 <42347333+Aris221@users.noreply.github.com>
2019-04-16 08:38:16 +0000

    Ajout d'article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,34 @@ export class AppComponent {
   posts = [
     {
       titlePost: 'Mon premier article',
-      content: 'contenu de mon article',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas, ' +
+        'lacus et tincidunt mattis, mauris lectus mattis ante, sed ultrices quam est nec tortor. ' +
+        'Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vitae turpis porta, ' +
+        'viverra ligula eget, lacinia nisi. Nulla commodo euismod elit, in rutrum lorem rhoncus sed. ' +
+        'Morbi ac ex non nisi interdum lobortis sit amet in turpis. Quisque aliquet, turpis at aliquam iaculis, ' +
+        'erat urna rhoncus tellus, id blandit sem justo id arcu. Duis posuere turpis ac mauris finibus accumsan.',
       loveIts: 0,
       created_at: Date(),
     },
     {
       titlePost: 'Mon deuxième article',
-      content: 'contenu de mon article',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas, ' +
+        'lacus et tincidunt mattis, mauris lectus mattis ante, sed ultrices quam est nec tortor. ' +
+        'Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vitae turpis porta, ' +
+        'viverra ligula eget, lacinia nisi. Nulla commodo euismod elit, in rutrum lorem rhoncus sed. ' +
+        'Morbi ac ex non nisi interdum lobortis sit amet in turpis. Quisque aliquet, turpis at aliquam iaculis, ' +
+        'erat urna rhoncus tellus, id blandit sem justo id arcu. Duis posuere turpis ac mauris finibus accumsan.',
       loveIts: 0,
       created_at: Date(),
     },
     {
       titlePost: 'Mon troisième article',
-      content: 'contenu de mon article',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas, ' +
+        'lacus et tincidunt mattis, mauris lectus mattis ante, sed ultrices quam est nec tortor. ' +
+        'Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vitae turpis porta, ' +
+        'viverra ligula eget, lacinia nisi. Nulla commodo euismod elit, in rutrum lorem rhoncus sed. ' +
+        'Morbi ac ex non nisi interdum lobortis sit amet in turpis. Quisque aliquet, turpis at aliquam iaculis, ' +
+        'erat urna rhoncus tellus, id blandit sem justo id arcu. Duis posuere turpis ac mauris finibus accumsan.',
       loveIts: 0,
       created_at: Date(),
     }
